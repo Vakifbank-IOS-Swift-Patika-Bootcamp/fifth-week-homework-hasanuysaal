685e634a9cb092e6c48cdc5e571d6a805b406eb5
hasanuysaal <uysalhasan32@gmail.com>
2022-11-21 15:29:13 +0300

    Some folders and CharactersCollectionViewCell created

---
+++ BreakingBadApp/BreakingBadApp/View/CollectionViews/CharactersCollectionView/CharactersCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersCollectionViewCell.swift
+//  BreakingBadApp
+//
+//  Created by Hasan Uysal on 21.11.2022.
+//
+
+import UIKit
+
+class CharactersCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+}
