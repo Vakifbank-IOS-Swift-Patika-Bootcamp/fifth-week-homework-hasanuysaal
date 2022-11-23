115e24de1366352078978ea2cb0d4a3201ab89d0
hasanuysaal <uysalhasan32@gmail.com>
2022-11-23 23:18:44 +0300

    Character model changed to fetch character id, Quotes Model created, getQuotes method added to Client,character detail showed to CharacterDetailsView, QuotesTableViewCell created and show quotes on CharacterQuotesView

---
+++ BreakingBadApp/BreakingBadApp/Model/Quotes.swift
@@ -0,0 +1,8 @@
+//
+//  Quotes.swift
+//  BreakingBadApp
+//
+//  Created by Hasan Uysal on 23.11.2022.
+//
+
+import Foundation
---
+++ BreakingBadApp/BreakingBadApp/View/TableViews/Quotes/QuotesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  QuotesTableViewCell.swift
+//  BreakingBadApp
+//
+//  Created by Hasan Uysal on 23.11.2022.
+//
+
+import UIKit
+
+class QuotesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
